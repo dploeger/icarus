afced8eea0710d4c9b65719d2036d7458795ab71
Dennis Plöger <develop@dieploegers.de>
2026-01-27 12:20:23 +0100

    fix: Ensure no duplicate properties will be produced

--- pkg/processors/copy_property.go
+++ pkg/processors/copy_property.go
@@ -2,6 +2,7 @@ package processors
 
 import (
 	"github.com/emersion/go-ical"
+	"golang.org/x/exp/maps"
 )
 
 // The CopyPropertyProcessor converts all-day events to timed events or vice versa
@@ -22,8 +23,13 @@ func (c *CopyPropertyProcessor) Process(input ical.Calendar, output *ical.Calend
 			source := event.Props.Get(c.SourceProperty)
 			target := event.Props.Get(c.TargetProperty)
 			if source != nil && (target == nil || c.Overwrite) {
-				source.Name = c.TargetProperty
-				event.Props.Set(source)
+				if target != nil {
+					event.Props.Del(c.TargetProperty)
+				}
+				target := ical.NewProp(c.TargetProperty)
+				target.Value = source.Value
+				target.Params = maps.Clone(source.Params)
+				event.Props.Add(target)
 			}
 		}
 		output.Children = append(output.Children, event.Component)
